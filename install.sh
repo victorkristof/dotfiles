b9c1a35b13781a50e89d854be37f3d70cb92df19
Victor Kristof <victor.kristof@epfl.ch>
2019-10-19 12:54:08 +0200

    Update install script.

--- install.sh
+++ install.sh
@@ -1,49 +1,65 @@
 #!/bin/sh
 
-# Shell
+### SHELL
+
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 source ~/.bash_profile
 
-# Input
+### INPUT
+
 ln -s ~/dotfiles/inputrc ~/.inputrc
 source ~/.inputrc
 
-# Tmux
-# Install conditionally on macOS or Ubuntu
+### GIT
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+### TMUX
+
+# Install conditionally on macOS or Ubuntu.
 if [ "$(uname)" == "Darwin" ]; then
     ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 elif [ "$(uname)" == "Linux" ]; then
-    # elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     ln -s ~/dotfiles/tmux-ubuntu.conf ~/.tmux.conf
 fi
 cp ~/dotfiles/tmux-status-line.conf ~/.tmux/tmux-status-line.conf
-# Install tmux plugin manager
+# Install tmux plugin manager.
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Sqlite
-ln -s ~/dotfiles/sqliterc ~/.sqliterc
+### VIM
 
-# Vim
-mkdir ~/.vim
-mkdir ~/.vim/undo
+mkdir -p ~/.vim/undo
 ln -s ~/dotfiles/vimrc ~/.vimrc
-
-# Git
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-
-# Install VimPlug
+# Install VimPlug.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install all plugins
+# Install all plugins.
 vim +PlugInstall +qall
 
-# Python
+### SQLITE
+ln -s ~/dotfiles/sqliterc ~/.sqliterc
 
+### PYTHON
+
+# Install utils.
 mkdir ~/GitHub
 cd ~/GitHub
 git clone https://github.com/victorkristof/utils.git
+
+### BAT
+
+# Install with homebrew.
+brew install bat
+# Setup Solarized theme.
+mkdir -p .config/bat/themes
+cd Solarized
+# Checkout to correct theme definition.
+git checkout 87e01090cf
+cd ..
+# Enable theme.
+bat cache --build
+# Enable config.
+ln -s ~/dotfiles/batrc ~/.batrc
 
 echo "Welcome home."
